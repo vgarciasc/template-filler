495194b6f392a12bc131dbb213ecca4d72d4bece
littlevini\little vini <vingarciasc@gmail.com>
2017-02-09 23:36:10 -0200

    v0.3.0
    - css básico ainda não funcional;
    - imagens selecionadas individualmente por click ao invés de select menu
    no HTML;
    - opções de 'clear' e 'remove' pras seleções.

--- test.js
+++ test.js
@@ -11,6 +11,8 @@ var currentRectDest = new coord(0, 0);
 
 var actionHistory = [];
 
+var selectedRectIndex = -1;
+
 var currentMousePos = new coord(0, 0);
 
 function action(rectCreation, imageSet, rectIndex) {
@@ -55,16 +57,20 @@ function toggleEdit() {
 }
 
 function cleanCanvas() {
+	canvas.width = template.width;
+	canvas.height = template.height;
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function resetCanvas() {
 	rectList = [];
+	updateSelectList();
 	cleanCanvas();
 }
 
 function draw() {
 	cleanCanvas();
+	drawBackground();
 	context.drawImage(template, 0, 0);
 
 	if (nowSelecting && nowEditing) {
@@ -86,7 +92,20 @@ function draw() {
 		if (rectList[i].img) {
 			if (nowEditing) {
 				context.globalAlpha = 0.5;
-			}
+				if (i == selectedRectIndex) {
+					context.fillStyle = "rgba(0, 0, 200, 0.5)";
+					context.strokeStyle = "rgb(0, 0, 180)";
+					context.lineWidth = 5;
+					context.fillRect(rectList[i].x,
+						rectList[i].y,
+						rectList[i].width,
+						rectList[i].height);
+					context.strokeRect(rectList[i].x,
+						rectList[i].y,
+						rectList[i].width,
+						rectList[i].height);
+				}
+			}	
 			context.drawImage(rectList[i].img,
 				rectList[i].x,
 				rectList[i].y,
@@ -103,8 +122,14 @@ function draw() {
 			}
 		}
 		else if (nowEditing) {
-			context.fillStyle = "rgba(200, 200, 0, 0.5)";
-			context.strokeStyle = "rgb(180, 180, 0)";
+			if (i == selectedRectIndex) {
+				context.fillStyle = "rgba(0, 0, 200, 0.5)";
+				context.strokeStyle = "rgb(0, 0, 180)";
+			}
+			else {
+				context.fillStyle = "rgba(200, 200, 0, 0.5)";
+				context.strokeStyle = "rgb(180, 180, 0)";
+			}
 			context.lineWidth = 5;
 			context.fillRect(rectList[i].x,
 							rectList[i].y,
@@ -125,12 +150,17 @@ function draw() {
 
 	if (nowEditing) {
 		context.font = "20px Arial";
-		context.fillStyle = "rgba(255, 0, 0, 0.5)"
+		context.fillStyle = "rgba(255, 0, 0, 0.5)";
 		context.textAlign = "left";
 		context.fillText("EDIT MODE",
 						0,
 						20);
 	}
+}
+
+function drawBackground() {
+	context.fillStyle = "rgb(255, 255, 255)";
+	context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function onKeyPress(e) {
@@ -153,12 +183,38 @@ function undo() {
 }
 
 function onMouseDown() {
-	nowSelecting = true;
 	var pos = getCursorPosition();
+	var hover = mouseOverRect();
+	if (hover != -1) {
+		if (selectedRectIndex == -1 ||
+			selectedRectIndex != hover) {
+			selectedRectIndex = hover;
+		}
+		else {
+			selectedRectIndex = -1;
+		}
+		return;
+	}
+
+	nowSelecting = true;
 	currentRectOrigin = new coord(pos.x, pos.y);
 }
 
+function mouseOverRect() {
+	var pos = getCursorPosition();
+	for (var i = 0; i < rectList.length; i++) {
+		if (pos.x > rectList[i].x && pos.x < rectList[i].x + rectList[i].width &&
+		pos.y > rectList[i].y && pos.y < rectList[i].y + rectList[i].height) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
 function onMouseUp() {
+	if (mouseOverRect() != -1) return;
+
 	nowSelecting = false;
 	var pos = getCursorPosition();
 
@@ -192,11 +248,11 @@ function onMouseUp() {
 }
 
 function updateSelectList() {
-	var $select = $("#selected_rect");
+	/*var $select = $("#selected_rect");
 	$select.empty();
 	$.each(rectList, function(key, value) {
 		$select.append($("<option></option>").attr("value", value.id).text("rect " + value.id));
-	});
+	});*/
 }
 
 function onMouseMove() {
@@ -212,18 +268,54 @@ function getCursorPosition() {
 }
 
 function getTemplate() {
-	template.src = templateurl.value; 
+	template.src = templateurl.value;
+	resetCanvas();
 }
+
+function getSelectedRectangle() {
+	//var sel = document.getElementById("selected_rect");
+	//sel = sel[sel.selectedIndex].value;
+	//return sel - 1;
+
+	return selectedRectIndex;
+} 
 
 function setImage() {
 	var topleft, width, height;
 	width = Math.abs(currentRectDest.x - currentRectOrigin.x);
 	height = Math.abs(currentRectDest.y - currentRectOrigin.y);
 
-	var sel = document.getElementById("selected_rect");
-	sel = sel[sel.selectedIndex].value;
-	rectList[sel - 1].img = new Image();
-	rectList[sel - 1].img.src = imageurl.value;
+	var sel = getSelectedRectangle();
+	if (sel != -1) {
+		rectList[sel].img = new Image();
+		rectList[sel].img.src = imageurl.value;
 
-	actionHistory[actionHistory.length] = new action(false, true, sel - 1);
+		actionHistory[actionHistory.length] = new action(false, true, sel);
+	}
+}
+
+function removeSelectedRect() {
+	var sel = getSelectedRectangle();
+
+	if (sel != -1) {
+		for (var i = sel; i < rectList.length - 1; i++) {
+			rectList[i] = rectList[i + 1];
+			rectList[i].id -= 1;
+		}
+
+		rectList.length -= 1;
+		resetSelectedRect();
+		updateSelectList();
+	}
+}
+
+function clearSelectedRect() {
+	var sel = getSelectedRectangle();
+	if (sel != -1) {
+		rectList[sel].img = null;
+	}
+}
+
+function resetSelectedRect() {
+	selectedRectIndex = -1;
 }
